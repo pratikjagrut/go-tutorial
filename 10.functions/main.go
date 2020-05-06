b785a38fc82f272898db96d8bbcb87fbb783bc23
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-06 23:56:32 +0530

    variadic functions

--- 10.functions/main.go
+++ 10.functions/main.go
@@ -14,7 +14,40 @@ func add(i, j, k int) int {
 	return i + j + k
 }
 
+//multiple return value
+func multiply(i, j int) (int, int, int) {
+	return i, j, i * j
+}
+
+//Named return values
+func calc(i int) (x, y int) {
+	x = i / 10
+	y = i % 10
+	return
+}
+
+//Variadic Functions, can be called with any number of trailing arguments.
+func total(nums ...int) (total int) {
+	for _, i := range nums {
+		total += i
+	}
+	return total
+}
+
 func main() {
 	fmt.Println(addition(4, 5))
+
 	fmt.Println(add(4, 5, 6))
+
+	//multiple return value
+	i, j, k := multiply(4, 5)
+	fmt.Printf("%d * %d= %d\n", i, j, k)
+
+	//Named return values
+	x, y := calc(17)
+	fmt.Printf("Quotient: %d, Remainder: %d\n", x, y)
+
+	//Variadic Functions
+	fmt.Println("Total: ", total(1, 2, 3, 4, 5))
+	fmt.Println("Total: ", total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 }
