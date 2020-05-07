6adde30fe125af3ac1942b18e99cc6a6f0fa35b8
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-07 10:13:33 +0530

    Structures

---
+++ 13_structs/main.go
@@ -0,0 +1,49 @@
+//A struct is a collection of fields.
+package main
+
+import "fmt"
+
+type student struct {
+	name   string
+	rollNo int
+}
+
+// This are struct literal which denotes a newly allocated struct value by listing the values of its fields.
+var (
+	s1 = student{"Harry", 1}  // has type student
+	s2 = student{name: "Ron"} // rollNo:0 is implicit
+	s3 = student{}            // name: "", rollNo: 0 is implicit
+	s4 = &student{"Rick", 2}  // has type *student
+)
+
+//Go supports methods defined on struct types.
+func (s *student) updateStudent(name string, rollNo int) {
+	s.name = name
+	s.rollNo = rollNo
+}
+
+func (s student) getStudentInfo() (string, int) {
+	return s.name, s.rollNo
+}
+
+func main() {
+	//Print struct literals
+	fmt.Println(s1, s2, s3, s4)
+
+	// accessing fields
+	s := student{"Jessica Rich", 15}
+	fmt.Println(s)
+	s.name = "Jessica"
+	fmt.Println(s)
+
+	//access using pointer
+	p := &s
+	fmt.Println(p.name, p.rollNo)
+
+	//methods on struct
+	s.updateStudent("Richie", 101)
+	fmt.Println(s)
+
+	name, rollNo := s.getStudentInfo()
+	fmt.Println(name, rollNo)
+}
