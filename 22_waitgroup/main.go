360039d9e5a7a77a255b1b4c102a119b436409d7
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-08 20:06:10 +0530

    waitgroup

---
+++ 22_waitgroup/main.go
@@ -0,0 +1,25 @@
+//A WaitGroup is used to wait for a collection of Goroutines to finish executing.
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func goroutine(id int, wg *sync.WaitGroup) {
+	fmt.Printf("Goroutine number %d started\n", id)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Goroutine number %d stoped\n", id)
+	wg.Done() //Decreases the counter
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i < 4; i++ {
+		wg.Add(1) //Increments the counter
+		go goroutine(i, &wg)
+	}
+	wg.Wait() //makes the main goroutine to wait until the counter becomes zero
+	fmt.Println("All goroutines finished their execution")
+}
