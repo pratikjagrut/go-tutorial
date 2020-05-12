924989318965d647674201acf6e27de828e5d490
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-12 13:56:56 +0530

    error handling

--- 35_writing_files/main.go
+++ 35_writing_files/main.go
@@ -7,39 +7,43 @@ import (
 	"os"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
 	//writing bytes in file
 	data := []byte("hello\nworld!\n")
 	//0644 is linux file permissions, if file does not exitst it will create with thos specified permission
 	err := ioutil.WriteFile("data", data, 0644)
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 
 	f, err := os.Create("data2")
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 
 	defer f.Close()
 
 	str := "Go\n"
 	data2 := []byte(str)
 	n2, err := f.Write(data2)
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("wrote %d bytes\n", n2)
 
 	n3, err := f.WriteString("Is\n")
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("wrote %d bytes\n", n3)
 
 	f.Sync() //Issue a Sync to flush writes to stable storage.
 
 	w := bufio.NewWriter(f)
 	n4, err := w.WriteString("Fun!\n")
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("wrote %d bytes\n", n4)
 
 	w.Flush()
