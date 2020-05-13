a1f9b8651455eddf08892fd9e0b8f3ca0747f23c
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-14 00:25:19 +0530

    context with http

---
+++ 46_context_with_http/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"context"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second) //it will cancel http req after 3 sec
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8081", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		log.Fatal(res.Status)
+	}
+
+	io.Copy(os.Stdout, res.Body)
+}
