f67685ea1b1eab1bd4b6478dc9d69cd39d0fcfb9
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-13 10:43:29 +0530

    http client

---
+++ 43_HTTP_client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	//Issue an HTTP GET request to a server
+	resp, err := http.Get("https://golang.org/doc/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close() //Close Body at the end
+
+	// Print the HTTP response status.
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 10; i++ { //Prints 10 lines from resp.Body
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil { //checks for error
+		panic(err)
+	}
+}
