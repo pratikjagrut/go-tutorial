23d7b209e58f14eda7c5a2b42c494f57c6c6112c
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-05-13 10:40:31 +0530

    cmd line flags

---
+++ 40_cmd_line_flags/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	//Basic flag declarations are available for string, integer, and boolean options
+	//(flagName, defaultValue, usage)
+	//This flag function retruns pointer to the value not the value itself
+	stringPtr := flag.String("word", "foo", "a string")
+	intPtr := flag.Int("number", 42, "an int")
+	boolPtr := flag.Bool("ok", false, "a bool")
+
+	//Also we can decalare option that uses an existing variable
+	var extVar string
+	flag.StringVar(&extVar, "extvar", "bar", "a string var")
+
+	//After all flags are declared, flag.Parse() is called to execute the command-line parsing.
+	flag.Parse()
+
+	fmt.Println("Word:", *stringPtr)
+	fmt.Println("Number:", *intPtr)
+	fmt.Println("Ok:", *boolPtr)
+	fmt.Println("ExtVar:", extVar)
+
+	//This will collect all tailing var or positional arg
+	//Note that the flag package requires all flags to appear before positional arguments
+	//(otherwise the flags will be interpreted as positional arguments).
+	fmt.Println("Tail:", flag.Args())
+
+	// To run, try some cases from below
+	// go run main.go
+	// go run main.go --help
+	// go run main.go -word=life -number=8 -ok=true -extvar=hello a1 b2 c3 d4
+	// go run main.go -word=life -number=8 a1 b2 c3 d4 -ok=true
+	// go run main.go -word=life a1 b2 c3 d4
+}
