c1c9dcb0296907884b458f05b9c9e6869f69bbb0
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2020-10-03 23:20:39 +0530

    Add numeric types range (#2)
    Add numeric types range

---
+++ 46_numeric_types_range/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Constant
+const (
+	MaxUint = ^uint(0)
+	MinUint = 0
+	MaxInt  = int(^uint(0) >> 1)
+	MinInt  = -MaxInt - 1
+)
+
+func main() {
+	fmt.Println("Integer range on this computer")
+	fmt.Println("MinUint:", MinUint)
+	fmt.Println("MaxUint:", MaxUint)
+	fmt.Println("MinInt:", MinInt)
+	fmt.Println("MaxInt:", MaxInt)
+}
